2915c07b42794b0eeb6e750545ebe1d51129b2ad
Michael <1902276@student.uwtsd.ac.uk>
2022-08-01 15:57:37 +0100

    Seed toggle

--- Scripts/Lowbar.gd
+++ Scripts/Lowbar.gd
@@ -1,5 +1,3 @@
 extends Control
 
 
-func _process(_delta):
-	$WorldInfo/Seed.text = str(Global.world_seed)
--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -1,11 +1,14 @@
 extends Control
 
 var is_paused = false setget set_is_paused
+#var seed_displayed = false
 
 
 func _process(_delta):
 	if Input.is_action_just_pressed("pause"):
 		self.is_paused = !is_paused
+		
+	$WorldInfo/Seed.text = str(Global.world_seed)
 
 
 func set_is_paused(value):
@@ -20,3 +23,7 @@ func _on_Resume_pressed():
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
+
+func _on_ViewSeed_pressed():
+	$WorldInfo.visible = !$WorldInfo.visible
