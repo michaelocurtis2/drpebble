f350743121383c064629c245a5de965f5bf150ae
Michael <1902276@student.uwtsd.ac.uk>
2022-08-04 02:52:39 +0100

    Cheat menu

--- Scripts/Lowbar.gd
+++ Scripts/Lowbar.gd
@@ -1,3 +1,17 @@
 extends Control
 
+var is_cheating = false setget set_is_cheating
 
+
+func _process(_delta):
+	if Input.is_action_just_pressed("cheat"):
+		self.is_cheating = !is_cheating
+
+
+func set_is_cheating(value):
+	is_cheating = value
+	visible = is_cheating
+
+
+func _on_Button_pressed():
+	Global.tool_damage = 8
